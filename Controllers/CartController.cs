326ae75a78b38c910e7023473472a3cef3891a4c
skoc39 <Skoc_ol39@student.itstep.org>
2023-08-13 22:35:25 +0300

    Update Controller and Services

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,19 +1,3 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing.Constraints;
-using System.Drawing;
-using AutoMapper;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Mvc.Formatters;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using SmartBuyAPI.Helpers;
-using SmartBuyApi.Data.DataBase.Tables;
-using SmartBuyApi.Data.Models.DTO.Category;
-using SmartBuyApi.DataBase;
-using SmartBuyApi.Data.Services;
-
 namespace SmartBuyAPI.Controllers
 {
     [Route("api/[controller]")]
