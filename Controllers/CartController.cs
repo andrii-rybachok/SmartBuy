77495a9ba102c71d5828c500f9f559f313fa408b
skoc39 <Skoc_ol39@student.itstep.org>
2023-08-13 20:39:03 +0300

    Update Controller

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -12,31 +12,61 @@ using SmartBuyAPI.Helpers;
 using SmartBuyApi.Data.DataBase.Tables;
 using SmartBuyApi.Data.Models.DTO.Category;
 using SmartBuyApi.DataBase;
+using SmartBuyApi.Data.Services;
 
 namespace SmartBuyAPI.Controllers
 {
+    [Route("api/[controller]")]
+        [ApiController]
     public class CartController : Controller
     {
-        private readonly ApplicationDbContext _db;
+            private readonly ICartService _service;
+            public CartController(ICartService service)
+            {
+                _service = service;
+            }
 
-        public CartController(ApplicationDbContext db)
-        {
-            _db = db;
+            [HttpGet]
+            public IActionResult Get()
+            {
+                var items = _service.GetAll();
+                return Ok(items);
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult Get(string id)
+            {
+                var item = _service.GetItem(id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                return Ok(item);
+            }
+
+            [HttpPost]
+            public IActionResult Post([FromBody] CartItem value)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var item = _service.AddItem(value);
+                return CreatedAtAction("Get", new { id = item.Id }, item);
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult Remove(Guid id)
+            {
+                var existingItem = _service.GetItem(id);
+                if (existingItem == null)
+                {
+                    return NotFound();
+                }
+                _service.RemoveItem(id);
+                return NoContent();
+            }
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddToCart(Cart item)
-        {
-            _db.Cart.Add(item);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
 
     }
 }
